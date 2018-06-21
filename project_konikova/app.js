c25536ab63da3371c9fa79705ee65b292ed5edfc
bluezkate <fmfanka@gmail.com>
2018-06-21 15:43:45 +0300

    carousel semi-done, navbar minor fixes

--- project_konikova/app.js
+++ project_konikova/app.js
@@ -1,5 +1,18 @@
 var owl = $(".painting-carousel");
 owl.owlCarousel({
     items: 1,
-    pagination: false 
+    pagination: false,
+    navigation: true,
+    navigationText : ["<img src='img/main/arrow-right.svg'>", "<img src='img/main/arrow-right.svg'>"],
+    rewindNav : false,
+    URLhashListener: true,
 });
+
+// $(".next").click(function(){
+//   owl.trigger('owl.next');
+// })
+// $(".prev").click(function(){
+//   owl.trigger('owl.prev');
+// })
+
+// document.getElementById('painting-interior').innerHTML = '<img src="img/carousel/54b68e50b96943f5cae0a93bd0820307.jpg" alt="Демон сидящий-оригинал">';
