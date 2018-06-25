037e01eb244a5cefe3822cc44ff4377c58c65503
Andrey <alexey@MacBook-Aleksej.local>
2018-06-26 00:45:20 +0300

    finish js for popup

--- project_morozov/js/popup.js
+++ project_morozov/js/popup.js
@@ -1,17 +1,20 @@
 const   save_copy = document.getElementById('save-copy'),
+        add_to_collect = document.getElementById('add-collect'),
         overlay = document.getElementById('overlay'),
-        popup_form = document.getElementById('popup-form');
+        popup_copy = document.getElementById('popup-copy'),
+        popup_add = document.getElementById('popup-add');
 
-fadeIn = () => {
+let fadeIn = (element) => {
     overlay.classList.add('visible');
-    popup_form.classList.add('visible');
+    element.classList.add('visible');
 };
 
-fadeOut = () => {
-    popup_form.classList.remove('visible');
+let fadeOut = (element) => {
+    element.classList.remove('visible');
     overlay.classList.remove('visible');
 };
 
-save_copy.addEventListener('click', fadeIn);
-
-overlay.addEventListener('click', fadeOut);
+save_copy.addEventListener('click', fadeIn.bind(null, popup_copy));
+add_to_collect.addEventListener('click', fadeIn.bind(null, popup_add));
+overlay.addEventListener('click', fadeOut.bind(null, popup_copy));
+overlay.addEventListener('click', fadeOut.bind(null, popup_add));
