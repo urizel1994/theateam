7f143842ac80443ee928f32d76e731ba183660fd
Ivan_Kravcov <gfdaker96@gmail.com>
2018-07-17 19:11:45 +0300

    first part

---
+++ project_kravcov/JS_Homework/homeWork_1.js
@@ -0,0 +1,9 @@
+// 1. Займёмся арифметикой
+// 1.1
+let allStudents = 80,
+	not100percent = ((allStudents/2)-4)/3*2;
+	console.log(not100percent);
+// 1.2
+	console.log(( 9 + 1 ) + (( 1 - ( 2017 % 2 )) * 6));
+// 1.3
+	console.log((1+2+2*2+2*2*2)*5);
